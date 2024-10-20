df69408c42a4f6525b78b17c6532e8ea76e0659e
Finn St John <stjohn.finn@gmail.com>
2024-10-19 19:26:26 -0700

    add super basic display for word GA

--- dist/wordGeneticAlgorithm.js
+++ dist/wordGeneticAlgorithm.js
@@ -1,5 +1,5 @@
-const CHARS = "abcdefghijklmnopqrstuvwxyz";
-const TARGET_STRING = "wally";
+const CHARS = "abcdefghijklmnopqrstuvwxyz ";
+const TARGET_STRING = "rice and weird";
 const TARGET_STRING_LENGTH = TARGET_STRING.length;
 const mutationChance = 0.02;
 class WordOrganism {
@@ -72,15 +72,54 @@ function stepFunction(model) {
 }
 let geneticAlgorithm = new GeneticAlgorithm(createOrganism, stepFunction, calculateFitness, crossover, mutate, shouldTerminate, shouldProgressGeneration);
 console.log(geneticAlgorithm);
+let environment = document.createElement("div");
+environment.style.display = "grid";
+environment.style.gridTemplateColumns = "1fr 1fr 1fr 1fr";
+environment.style.gap = "0.5rem";
+environment.style.overflowY = "scroll";
+environment.style.justifyItems = "center";
+environment.style.fontSize = "0.5rem";
+environment.style.width = "100%";
 let playButton = document.createElement("button");
 document.querySelector("body").appendChild(playButton);
-playButton.innerText = "play";
+playButton.innerText = "▶ play ";
 playButton.addEventListener("click", () => {
     geneticAlgorithm.play();
 });
 let pauseButton = document.createElement("button");
 document.querySelector("body").appendChild(pauseButton);
-pauseButton.innerText = "pause";
+pauseButton.innerText = "⏸ pause";
 pauseButton.addEventListener("click", () => {
     geneticAlgorithm.pause();
 });
+let controls = document.createElement("div");
+controls.appendChild(playButton);
+controls.appendChild(pauseButton);
+controls.style.display = "flex";
+controls.style.flexDirection = "row";
+controls.style.alignItems = "flex-start";
+controls.style.justifyContent = "space-evenly";
+controls.style.width = "100%";
+let view = document.createElement("div");
+view.appendChild(controls);
+view.appendChild(environment);
+view.style.display = "flex";
+view.style.flexDirection = "column";
+view.style.alignItems = "center";
+view.style.gap = "1rem";
+document.querySelector("#view").appendChild(view);
+function displayWordGeneticAlgorithm(model) {
+    environment.innerHTML = "";
+    for (const word of model.population) {
+        let wordElement = document.createElement("p");
+        wordElement.innerText = String(word.genes);
+        environment.appendChild(wordElement);
+        if (word.genes == TARGET_STRING) {
+            wordElement.style.color = "green";
+        }
+    }
+    requestAnimationFrame(() => {
+        displayWordGeneticAlgorithm(geneticAlgorithm.model);
+    });
+}
+displayWordGeneticAlgorithm(geneticAlgorithm.model);
